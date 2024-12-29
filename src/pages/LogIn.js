9dfb530b9f737aa72dc4fe32bbbf5c3c58975614
Courtney Greer <courtneygreer@Courtneys-MacBook-Pro.local>
2024-12-29 00:07:26 -0500

    added forms for login and signup

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,15 +1,29 @@
-import React from 'react'
-import '../stylesheets/LogIn.css';
+// src/pages/Login.js
 
-const  Login = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../stylesheets/Auth.css';
+
+function Login() {
   return (
-    <>
-    <div className='hero'>
-        <h1>Log In Here</h1>
+    <div className="auth-container">
+      <h1>Login</h1>
+      <form className="auth-form">
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Enter your email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter your password" />
+        </div>
+        <button type="button" className="auth-button">Log In</button>
+      </form>
+      <p>
+        Don't have an account? <Link to="/signup">Sign Up</Link>
+      </p>
     </div>
-    </>
-  )
-};
+  );
+}
 
 export default Login;
-
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import '../stylesheets/SignUp.css';
+// src/pages/SignUp.js
 
-export default function SignUp() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../stylesheets/Auth.css';
+
+function SignUp() {
   return (
-    <div className='hero'>
-        <h1>Sign Up Here</h1>
+    <div className="auth-container">
+      <h1>Sign Up</h1>
+      <form className="auth-form">
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" placeholder="Enter your username" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Enter your email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter your password" />
+        </div>
+        <button type="button" className="auth-button">Sign Up</button>
+      </form>
+      <p>
+        Already have an account? <Link to="/login">Log In</Link>
+      </p>
     </div>
-  )
+  );
 }
+
+export default SignUp;
